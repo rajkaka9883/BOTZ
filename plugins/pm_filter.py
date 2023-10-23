2920508f4dbaa3584501f5b59e04db130b027f1a
rajkaka9883 <139911877+rajkaka9883@users.noreply.github.com>
2023-10-23 19:33:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -68,8 +68,8 @@ async def give_filter(client, message):
             return
         else:
             return await message.reply_text(
-                          text=f"<b>Hᴇʏ {message.from_user.mention}, {str(total_results)} ʀᴇsᴜʟᴛs ᴀʀᴇ ғᴏᴜɴᴅ ɪɴ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ғᴏʀ ʏᴏᴜʀ ᴏ̨ᴜᴇʀʏ {search}. \n\nTʜɪs ɪs ᴀ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ sᴏ ᴛʜᴀᴛ ʏᴏᴜ ᴄᴀɴ'ᴛ ɢᴇᴛ ғɪʟᴇs ғʀᴏᴍ ʜᴇʀᴇ...\n\nJᴏɪɴ ᴀɴᴅ Sᴇᴀʀᴄʜ Hᴇʀᴇ - @Full_Entertainment_Group</b>",
-                           reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📝 ʀᴇǫᴜᴇsᴛ ʜᴇʀᴇ ", url=f"https://t.me/Full_Entertainment_Group")]])
+                          text=f"<b>💭 Nᴀᴍᴀsᴛᴇ {message.from_user.mention}ᴊɪ🥰, {str(total_results)} ʀᴇsᴜʟᴛs ᴀʀᴇ ғᴏᴜɴᴅ ɪɴ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ғᴏʀ ʏᴏᴜʀ ᴏ̨ᴜᴇʀʏ {search}. \n\nTʜɪs ɪs ᴀ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ sᴏ ᴛʜᴀᴛ ʏᴏᴜ ᴄᴀɴ'ᴛ ɢᴇᴛ ғɪʟᴇs ғʀᴏᴍ ʜᴇʀᴇ...\n\nJᴏɪɴ ᴀɴᴅ Sᴇᴀʀᴄʜ Hᴇʀᴇ - @Full_Entertainment_Group</b>",
+                           reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔻 ꜱᴇᴀʀᴄʜ  ʜᴇʀᴇ 🔺", url=f"https://t.me/Full_Entertainment_Group")]])
                        )             
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pm_text(bot, message):
