3d6e32fd1cc1dfa8da7c770cd2488dc2000756d5
rajkaka9883 <139911877+rajkaka9883@users.noreply.github.com>
2023-11-07 15:05:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -71,28 +71,28 @@ async def give_filter(client, message):
                           text=f"<b>💭 Nᴀᴍᴀsᴛᴇ {message.from_user.mention}ᴊɪ🥰, {str(total_results)} ʀᴇsᴜʟᴛs ᴀʀᴇ ғᴏᴜɴᴅ ɪɴ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ғᴏʀ ʏᴏᴜʀ ᴏ̨ᴜᴇʀʏ {search}. \n\nTʜɪs ɪs ᴀ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ sᴏ ᴛʜᴀᴛ ʏᴏᴜ ᴄᴀɴ'ᴛ ɢᴇᴛ ғɪʟᴇs ғʀᴏᴍ ʜᴇʀᴇ...\n\nJᴏɪɴ ᴀɴᴅ Sᴇᴀʀᴄʜ Hᴇʀᴇ - @Full_Entertainment_Group</b>",
                            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔻 ꜱᴇᴀʀᴄʜ  ʜᴇʀᴇ 🔺", url=f"https://t.me/Full_Entertainment_Group")]])
                        )             
-@Client.on_message(filters.private & filters.text & filters.incoming)
-async def pm_text(bot, message):
-    content = message.text
-    user = message.from_user.first_name
-    user_id = message.from_user.id
-    if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
-    if user_id in ADMINS: return # ignore admins
-    await message.reply_text(
-         text=f"<b>ʜᴇʏ {user} 😍 ,\n\nʏᴏᴜ ᴄᴀɴ'ᴛ ɢᴇᴛ ᴍᴏᴠɪᴇs ꜰʀᴏᴍ ʜᴇʀᴇ. ʀᴇǫᴜᴇsᴛ ɪᴛ ɪɴ ᴏᴜʀ ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ</a> ᴏʀ ᴄʟɪᴄᴋ ʀᴇǫᴜᴇsᴛ ʜᴇʀᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ 👇</b>",   
-         reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton("🔻 ʀᴇǫᴜᴇsᴛ ʜᴇʀᴇ 🔺", url=f"https://t.me/MOVIES_REQUEST_NOW_2_0")
-            ],[
-                InlineKeyboardButton("🍟 Mᴏᴠɪᴇ Gʀᴏᴜᴘ 🍟", url=f"https://t.me/Full_Entertainment_Group")
-            ],[
-                InlineKeyboardButton('⁉️ ᴄʟᴏsᴇ ⁉️', callback_data='close_data')
-            ]]
-         )
-    )    
-    await bot.send_message(
-        chat_id=LOG_CHANNEL,
-        text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
-    )
+# @Client.on_message(filters.private & filters.text & filters.incoming)
+# async def pm_text(bot, message):
+#    content = message.text
+#   user = message.from_user.first_name
+#   user_id = message.from_user.id
+#   if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
+#    if user_id in ADMINS: return # ignore admins
+#    await message.reply_text(
+#         text=f"<b>ʜᴇʏ {user} 😍 ,\n\nʏᴏᴜ ᴄᴀɴ'ᴛ ɢᴇᴛ ᴍᴏᴠɪᴇs ꜰʀᴏᴍ ʜᴇʀᴇ. ʀᴇǫᴜᴇsᴛ ɪᴛ ɪɴ ᴏᴜʀ ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ</a> ᴏʀ ᴄʟɪᴄᴋ ʀᴇǫᴜᴇsᴛ ʜᴇʀᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ 👇</b>",   
+#         reply_markup=InlineKeyboardMarkup([[
+#               InlineKeyboardButton("🔻 ʀᴇǫᴜᴇsᴛ ʜᴇʀᴇ 🔺", url=f"https://t.me/MOVIES_REQUEST_NOW_2_0")
+#            ],[
+#                InlineKeyboardButton("🍟 Mᴏᴠɪᴇ Gʀᴏᴜᴘ 🍟", url=f"https://t.me/Full_Entertainment_Group")
+#            ],[
+#               InlineKeyboardButton('⁉️ ᴄʟᴏsᴇ ⁉️', callback_data='close_data')
+#            ]]
+#        )
+#    )    
+#    await bot.send_message(
+#       chat_id=LOG_CHANNEL,
+#        text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
+#    )
 
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
