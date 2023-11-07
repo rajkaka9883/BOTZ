3a3e36ae6a13e457ddc93a32ed794f7ae6debc8b
rajkaka9883 <139911877+rajkaka9883@users.noreply.github.com>
2023-11-07 21:05:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -78,8 +78,8 @@ async def pm_text(bot, message):
     user = message.from_user.first_name
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
-     if user_id in ADMINS: return # ignore admins
-     await message.reply_text(
+    if user_id in ADMINS: return # ignore admins
+    await message.reply_text(
           text=f"<b>ʜᴇʏ {user} 😍 ,\n\nʏᴏᴜ ᴄᴀɴ'ᴛ ɢᴇᴛ ᴍᴏᴠɪᴇs ꜰʀᴏᴍ ʜᴇʀᴇ. ʀᴇǫᴜᴇsᴛ ɪᴛ ɪɴ ᴏᴜʀ ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ</a> ᴏʀ ᴄʟɪᴄᴋ ʀᴇǫᴜᴇsᴛ ʜᴇʀᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ 👇</b>",   
           reply_markup=InlineKeyboardMarkup([[
                 InlineKeyboardButton("🔻 ʀᴇǫᴜᴇsᴛ ʜᴇʀᴇ 🔺", url=f"https://t.me/MOVIES_REQUEST_NOW_2_0")
